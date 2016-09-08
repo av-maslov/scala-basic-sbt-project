28f154ed1f3397a8101c2591f7603ee4221b4e06
Alexandr <maslov314@gmail.com>
2016-09-08 11:47:25 +0200

    Ok

---
+++ src/main/scala/hw2.scala
@@ -0,0 +1,20 @@
+/*
+If there is hw.scala in the root then:
+
+[warn] Multiple main classes detected.  Run 'show discoveredMainClasses' to see the list
+
+Multiple main classes detected, select one to run:
+
+ [1] Hi
+ [2] TestObject
+
+Enter number: 2
+*/
+
+/*
+
+object TestObject {
+  def main(args: Array[String]) = println("Hi from the test object!")
+}
+
+*/
