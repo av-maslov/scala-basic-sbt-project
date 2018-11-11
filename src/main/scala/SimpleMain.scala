86bd348f969587475dcfa2565f4f9433d5dea230
Alexandr <av-maslov@users.noreply.github.com>
2018-11-11 14:19:57 +0200

    Update SimpleMain.scala

--- src/main/scala/SimpleMain.scala
+++ src/main/scala/SimpleMain.scala
@@ -1,5 +1,22 @@
+import breeze.linalg._
+import breeze.plot._
+
 object SimpleMain {
-  def main(args: Array[String]):Unit = {
-		 println("***")
+	
+  def makePlot():Unit={
+    val f = Figure()
+    val p = f.subplot(0)
+    val x = linspace(0.0,1.0)
+    p += plot(x, x :^ 2.0)
+    p += plot(x, x :^ 3.0, '.')
+    p.xlabel = "x axis"
+    p.ylabel = "y axis"
+    f.saveas("lines.png")
   }
+
+  def main(args: Array[String]):Unit = {
+    println("***")
+    makePlot()
+  }
+
 }
